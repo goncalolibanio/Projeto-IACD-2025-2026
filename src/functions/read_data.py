e6dbaa11d2b1732783489d3b513518a686afdd9e
Caio712 <caio.santos@ubi.pt>
2026-05-12 23:18:08 +0100

    Join dos CSV

--- src/functions/read_data.py
+++ src/functions/read_data.py
@@ -7,5 +7,15 @@ def read_data():
     df_outcomes = pd.read_csv('data/outcomes.csv')
     df_patients = pd.read_csv('data/patients.csv')
 
-    return df_diets, df_nutritionists, df_outcomes, df_patients
+    # Integrar os dados usando a tabela 'outcomes' como base
+    df_merged = df_outcomes.merge(df_patients, on='patient_id', how='left')
+
+    # Juntar dados dos nutricionistas
+    df_merged = df_merged.merge(df_nutritionists, on='patient_id', how='left')
+
+    # juntar dados das dietas
+    df_merged = df_merged.merge(df_diets, on='diet_id', how='left')
+
+    # Guardar o dataset unido num novo ficheiro CSV
+    df_merged.to_csv('data/processed/integrated_dataset.csv', index=False)
 
