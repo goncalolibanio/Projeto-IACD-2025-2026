ba4670aedc46c4be97a85df0623b7d3082708299
goncalolibanio <goncalo.s.libanio@gmail.com>
2026-05-12 23:09:29 +0100

    Added read_data.py

--- src/functions/read_data.py
+++ src/functions/read_data.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 def read_data():
-    df_diets = pd.read_csv('../../data/diets.csv')
-    df_nutritionists = pd.read_csv('../../data/nutritionists.csv')
-    df_outcomes = pd.read_csv('../../data/outcomes.csv')
-    df_patients = pd.read_csv('../../data/patients.csv')
+
+    df_diets = pd.read_csv('data/diets.csv')
+    df_nutritionists = pd.read_csv('data/nutritionists.csv')
+    df_outcomes = pd.read_csv('data/outcomes.csv')
+    df_patients = pd.read_csv('data/patients.csv')
+
     return df_diets, df_nutritionists, df_outcomes, df_patients
 
