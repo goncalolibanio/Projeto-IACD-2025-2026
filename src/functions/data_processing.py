9492eb50af95d07757f82542ef1c22f82ee4c4b2
Caio712 <caio.santos@ubi.pt>
2026-05-19 21:54:12 +0100

    Gerir os outliers

--- src/functions/data_processing.py
+++ src/functions/data_processing.py
@@ -30,4 +30,25 @@ def data_processing(df: pd.DataFrame) -> pd.DataFrame:
     float_cols = df.select_dtypes(include='float').columns
     df[float_cols] = df[float_cols].round(3)
 
+    #Limites do Domínio
+
+    df['mean_adherence_pct'] = df['mean_adherence_pct'].clip(lower=0, upper=100)
+    df['age'] = df['age'].clip(lower=16, upper=100)
+    df['sleep_hours'] = df['sleep_hours'].clip(lower=2, upper=15)
+
+    #Metodo IQR (limitação)
+
+    cols_outliers_iqr = ['sodium_limit_mg', 'fiber_target_g', 'years_experience']
+
+    for col in cols_outliers_iqr:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+
+        limite_inferior = Q1 - 1.5 * IQR
+        limite_superior = Q3 + 1.5 * IQR
+
+        df[col] = df[col].clip(lower=limite_inferior, upper=limite_superior)
+
     return df
+
